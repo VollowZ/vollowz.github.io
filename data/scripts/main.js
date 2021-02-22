82b1dd61fc7f118b21d1e76330adc5cf82194945
Vollow_Z <vollow_z@outlook.com>
2021-02-22 11:52:16 +0800

    update main&index.html
    Vollow_Z

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -1,3 +1,10 @@
-document.onreadystatechange = function () {
-  console.log(document.readyState);
+function Get(elem_name) {
+  return document.querySelectorAll(elem_name)[0];
 }
+
+window.onload = function () {
+  Get(".progress").style.cssText = "transform: translateY(-5px);";
+  setTimeout(() => {
+    Get(".progress").remove();
+  }, 200);
+};

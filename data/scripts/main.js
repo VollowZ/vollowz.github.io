8da606f90784c7752dc9b69a8478f38d376ffeab
Vollow_Z <vollow_z@outlook.com>
2021-03-15 18:02:03 +0800

    Update data
    VollowZ

--- data/scripts/main.js
+++ data/scripts/main.js
@@ -12,6 +12,28 @@ window.onload = function () {
   setTimeout(() => {
     Get(".progress").remove();
   }, 200);
+
+  const rcm = document.querySelector(".right-click-menu");
+  rcm.classList.remove("open");
+  document.addEventListener("click", (e) => {
+    rcm.classList.remove("open");
+  });
+  rcm.addEventListener("click", (event) => {
+    var event = event || window.event;
+    event.cancelBubble = true;
+    rcm.classList.remove("open");
+  });
+  document.oncontextmenu = (event) => {
+    var event = event || window.event;
+    rcm.classList.add("open");
+    var l, t;
+    l = event.clientX;
+    t = event.clientY;
+    l = l >= (document.documentElement.clientWidth || document.body.clientWidth) - 260 ? (document.documentElement.clientWidth || document.body.clientWidth) - 260 : l;
+    t = t > (document.documentElement.clientHeight || document.body.clientHeight) - 200 ? (document.documentElement.clientHeight || document.body.clientHeight) - 200 : t;
+    rcm.style.cssText = `top: ${t}px;left: ${l}px;`;
+    return false;
+  };
 };
 
 window.addEventListener("scroll", function () {
